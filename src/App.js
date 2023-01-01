ff59896cd53df8c4f86e99068e578ccd65e796a6
yeon <ohy9205@naver.com>
2023-01-01 23:37:08 +0900

    라우팅 설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { Outlet } from "react-router";
+import Header from "./components/Header";
+
 function App() {
-  return <div className="App">APP</div>;
+  return (
+    <div className="App">
+      <Header />
+      <Outlet />
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [{ index: true, element: <Home /> }],
+  },
+  { path: "login", element: <Login /> },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
