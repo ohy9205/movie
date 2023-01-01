d2e045c516d88950249f787531d7ae0e5eeb5319
yeon <ohy9205@naver.com>
2023-01-02 04:42:53 +0900

    로그인 ui 수정

--- src/components/SignForm.jsx
+++ src/components/SignForm.jsx
@@ -1,14 +1,12 @@
 import React, { useState } from "react";
 import Button from "./ui/Button";
 
-export default function SignForm({ onSubmit, login, join }) {
+export default function SignForm({ onSubmit, isLogin }) {
   const [user, setUser] = useState({});
-  const title = (login && "로그인") || (join && "회원가입");
-  const btnText = (login && "로그인하기") || (join && "가입하기");
+  const btnText = isLogin ? "로그인하기" : "가입하기";
 
   const onChangeHandler = (e) => {
     const { name, value } = e.target;
-    console.log(e.target);
     setUser((state) => ({ ...state, [name]: value }));
   };
 
@@ -18,7 +16,6 @@ export default function SignForm({ onSubmit, login, join }) {
 
   return (
     <form onSubmit={onSubmitHandler}>
-      <h1>{title}</h1>
       <input
         type="text"
         name="userId"
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,19 @@
-import React from "react";
+import { useState } from "react";
+import SignForm from "../components/SignForm";
 
 export default function Login() {
-  return <div>Login</div>;
+  const [isLogin, setIsLogin] = useState(false);
+  const title = isLogin ? "로그인" : "회원가입";
+
+  const toggleLogin = () => {
+    setIsLogin((state) => !state);
+  };
+
+  return (
+    <section>
+      <h1>{title}</h1>
+      {<SignForm isLogin={isLogin} />}
+      <p onClick={toggleLogin}>아직 가입을 안하셨나요?</p>
+    </section>
+  );
 }
