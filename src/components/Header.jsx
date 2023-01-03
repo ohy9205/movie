7f9362ca698d8f7268bf082fac3c2d43a968fff9
yeon <ohy9205@naver.com>
2023-01-03 13:16:53 +0900

    Header추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,50 @@
 import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import { logoutUser } from "../api/firebase";
+import { useAuthContext } from "../store/AuthContext";
+import Button from "./ui/Button";
 
 export default function Header() {
-  return <div>Header</div>;
+  const { user } = useAuthContext();
+  const { pathname } = useLocation();
+
+  const userExist = (
+    <ul>
+      <li>
+        <Link to="/">Movies</Link>
+      </li>
+      <li>
+        <Button text="로그아웃" onClick={logoutUser} />
+      </li>
+    </ul>
+  );
+
+  const userNone = (
+    <ul>
+      <li>
+        {pathname === "/sign_in" && (
+          <Link to="/sign_up">
+            <Button text="회원가입" />
+          </Link>
+        )}
+        {pathname === "/sign_up" && (
+          <Link to="/sign_in">
+            <Button text="로그인" />
+          </Link>
+        )}
+      </li>
+    </ul>
+  );
+
+  return (
+    <header style={{ display: "flex" }}>
+      <Link to="/">
+        <h1>Movyes</h1>
+      </Link>
+      <nav>
+        {user && userExist}
+        {!user && userNone}
+      </nav>
+    </header>
+  );
 }
