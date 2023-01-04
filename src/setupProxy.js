ad8e5daacd07b7cd105b465c9d151811aad0b831
yeon <ohy9205@naver.com>
2023-01-04 23:14:02 +0900

    proxy설정

---
+++ src/setupProxy.js
@@ -0,0 +1,31 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = function (app) {
+  app.use(
+    "/search_json2.jsp",
+    createProxyMiddleware({
+      target: "http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api",
+      changeOrigin: true,
+    })
+  );
+  app.use(
+    "/searchDailyBoxOfficeList.json",
+    createProxyMiddleware({
+      target: "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice",
+      changeOrigin: true,
+    })
+  );
+
+  // app.use(
+  //   createProxyMiddleware("/search_json2.jsp", {
+  //     target: "http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api",
+  //     changeOrigin: true,
+  //   })
+  // );
+  // app.use(
+  //   createProxyMiddleware("/searchDailyBoxOfficeList.json", {
+  //     target: "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice",
+  //     changeOrigin: true,
+  //   })
+  // );
+};
