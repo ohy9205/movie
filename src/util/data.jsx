f74ce7792132f9d402d9f2935581a36b9135cde2
yeon <ohy9205@naver.com>
2023-01-04 23:17:23 +0900

    util메소드 정리

---
+++ src/util/data.jsx
@@ -0,0 +1,51 @@
+/** db에 저장할 데이터 추출 */
+export const dataFormat = (response) => {
+  const responseData = response.data.Data[0].Result;
+  const moviesData = responseData.map((data) => {
+    const {
+      movieSeq,
+      Codes: {
+        Code: [{ CodeNo }],
+      },
+      title,
+      genre,
+      repRlsDate,
+      prodYear,
+      posters,
+      plots: {
+        plot: [{ plotText }],
+      },
+      runtime,
+      rating,
+      nation,
+      titleEng,
+      titleOrg,
+      directors: {
+        director: [{ directorNm }],
+      },
+      actors: { actor },
+    } = data;
+
+    /**정규표현식으로  */
+    let titleForamt = title.replace(/!HS/g, "").replace(/!HE/g, "");
+
+    return {
+      id: movieSeq,
+      movieCode: CodeNo,
+      genre,
+      title: titleForamt,
+      releaseDate: repRlsDate,
+      prodYear,
+      poster: posters.split("|")[0],
+      summary: plotText,
+      runtime,
+      rating,
+      nation,
+      titleEng,
+      titleOrg,
+      director: directorNm,
+      actors: actor.map((it) => it.actorNm),
+    };
+  });
+  return moviesData;
+};
---
+++ src/util/date.jsx
@@ -0,0 +1,21 @@
+/** 오늘날짜 YYYYMMDD */
+export const getTodayDate = () => {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate() - 1;
+  return (
+    year + (month < 10 ? `0${month}` : month) + (day < 10 ? `0${day}` : day)
+  );
+};
+
+/** 날짜 포맷 */
+export const dateFormat = (date) => {
+  const newDate = new Date(date);
+  const year = newDate.getFullYear();
+  const month = newDate.month() + 1;
+  const day = newDate.getDate();
+  return `${year}-${month < 10 ? `0${month}` : month}-${
+    day < 10 ? `0${day}` : day
+  }`;
+};
