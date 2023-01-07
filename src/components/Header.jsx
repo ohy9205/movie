54fee3224b3112c034b834656ce61ef99abc271e
yeon <ohy9205@naver.com>
2023-01-08 06:19:29 +0900

    헤더 스타일링

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,56 +3,62 @@ import { Link, useLocation } from "react-router-dom";
 import { logoutUser } from "../api/firebase";
 import { useAuthContext } from "../store/auth/AuthContext";
 import Button from "./ui/Button";
+import { MdMovieFilter } from "react-icons/md";
+import styles from "./Header.module.css";
+import { BsEmojiHeartEyes } from "react-icons/bs";
 
 export default function Header({ pickCount }) {
   const { user } = useAuthContext();
   const { pathname } = useLocation();
 
   const userExist = (
-    <ul>
+    <>
       <li>
-        <Link to="/search">Search</Link>
+        <Link to="/search">검색</Link>
       </li>
       <li>
-        <Link to="/my_pick">
-          My pick <span>{pickCount}</span>
+        <Link to="/my_pick" className={styles.pick}>
+          <BsEmojiHeartEyes className={styles.pickIcon} />
+          <span className={styles.count}>{pickCount}</span>
         </Link>
       </li>
       <li>
-        <Link to="/community">Community</Link>
+        <Link to="/community">커뮤니티</Link>
       </li>
       <li>
         <Button text="로그아웃" onClick={logoutUser} />
       </li>
-    </ul>
+    </>
   );
 
   const userNone = (
-    <ul>
-      <li>
-        {pathname === "/sign_in" && (
-          <Link to="/sign_up">
-            <Button text="회원가입" />
-          </Link>
-        )}
-        {pathname === "/sign_up" && (
-          <Link to="/sign_in">
-            <Button text="로그인" />
-          </Link>
-        )}
-      </li>
-    </ul>
+    <li>
+      {pathname === "/sign_in" && (
+        <Link to="/sign_up">
+          <Button text="회원가입" />
+        </Link>
+      )}
+      {pathname === "/sign_up" && (
+        <Link to="/sign_in">
+          <Button text="로그인" />
+        </Link>
+      )}
+    </li>
   );
 
   return (
-    <header style={{ display: "flex" }}>
+    <header className={styles.header}>
       <Link to="/">
-        <h1>Movyes</h1>
+        <div className={styles.wrapper}>
+          <MdMovieFilter className={styles.titleIcon} />
+
+          <h1 className={styles.title}>Movyes</h1>
+        </div>
       </Link>
-      <nav>
+      <ul className={styles.nav}>
         {user && userExist}
         {!user && userNone}
-      </nav>
+      </ul>
     </header>
   );
 }
