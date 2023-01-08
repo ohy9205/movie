5821eda84deb904fc3fac2ab7b6bc643e917033b
yeon <ohy9205@naver.com>
2023-01-08 19:43:59 +0900

    무비 캐러셀 마크업

--- src/components/movie/MovieCard.jsx
+++ src/components/movie/MovieCard.jsx
@@ -3,6 +3,7 @@ import { useDispatch } from "react-redux";
 import { useAuthContext } from "../../store/auth/AuthContext";
 import { addPickFetch, removePickFetch } from "../../store/pick/pick-actions";
 import MovieDetailModal from "./MovieDetailModal";
+import styles from "./MovieCard.module.css";
 
 export default function MovieCard({ movie, isPick }) {
   const [isShow, setIsShow] = useState(false);
@@ -13,29 +14,36 @@ export default function MovieCard({ movie, isPick }) {
     setIsShow((showDetail) => !showDetail);
   };
 
+  // const moveSlide = (i) => {
+  //   let nextIndex = current + i;
+
+  //   if (nextIndex < 0) nextIndex = imgSize.current - 1;
+  //   else if (nextIndex >= imgSize.current) nextIndex = 0;
+
+  //   setCurrent(nextIndex);
+  // };
+
   return (
-    <>
-      <li style={{ width: "150px" }}>
-        <div onClick={isShowToggle}>
-          <img
-            src={movie.poster}
-            style={{ display: "block", width: "100%" }}
-            alt="X"
-          />
-          {movie.title}
-        </div>
-        {user && !isPick && (
-          <button onClick={() => dispatch(addPickFetch(user.uid, movie))}>
-            추가
-          </button>
-        )}
-        {user && isPick && (
-          <button onClick={() => dispatch(removePickFetch(user.uid, movie.id))}>
-            삭제
-          </button>
-        )}
-      </li>
+    <li className={styles.box}>
+      <div onClick={isShowToggle}>
+        <img
+          src={movie.poster}
+          style={{ display: "block", width: "100%" }}
+          alt="X"
+        />
+        {movie.title}
+      </div>
+      {user && !isPick && (
+        <button onClick={() => dispatch(addPickFetch(user.uid, movie))}>
+          추가
+        </button>
+      )}
+      {user && isPick && (
+        <button onClick={() => dispatch(removePickFetch(user.uid, movie.id))}>
+          삭제
+        </button>
+      )}
       {isShow && <MovieDetailModal movie={movie} onClick={isShowToggle} />}
-    </>
+    </li>
   );
 }
--- src/components/movie/MovieCarousel.jsx
+++ src/components/movie/MovieCarousel.jsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import MovieCard from "../movie/MovieCard";
+import styles from "./MovieCarousel.module.css";
+import Button from "../ui/Button";
 
 export default function MovieCarousel({ movies }) {
+  const [current, setCurrent] = useState(0);
+  const slideRef = useRef();
+
+  const onNextSlider = () => {
+    setCurrent((current) => current + 1);
+  };
+
+  const onPrevSlider = () => {
+    setCurrent((current) => current - 1);
+  };
+
   return (
-    <ul style={{ display: "flex" }}>
-      {movies.map((movie) => (
-        <MovieCard key={movie.id} movie={movie} />
-      ))}
-    </ul>
+    <div className={styles.box}>
+      <Button text={"<"} onClick={onPrevSlider} />
+      <div className={styles.carouselBox}>
+        <ul ref={slideRef} class={styles.boxInner}>
+          {movies.map((movie) => (
+            <MovieCard key={movie.id} movie={movie} />
+          ))}
+        </ul>
+      </div>
+      <Button text={">"} onClick={onNextSlider} />
+    </div>
   );
 }
