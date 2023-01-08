414300604347b8981bc4e7d6e616bbd03f9c6c5f
yeon <ohy9205@naver.com>
2023-01-09 03:58:13 +0900

    pick 페이지 스타일링

--- src/pages/Pick.jsx
+++ src/pages/Pick.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import MovieCard from "../components/movie/MovieCard";
+import styles from "./Pick.module.css";
 
 export default function Pick({ movies }) {
   return (
-    <section>
-      <h1>내가 '픽'한 영화</h1>
-      {movies &&
-        movies.map((movie) => (
-          <MovieCard key={movie.id} movie={movie} isPick />
-        ))}
+    <section className={styles.pick}>
+      <h1>내가 'Pick'한 영화</h1>
+      {movies && (
+        <ul className={styles.box}>
+          {movies.map((movie) => (
+            <MovieCard key={movie.id} movie={movie} isPick />
+          ))}
+        </ul>
+      )}
     </section>
   );
 }
