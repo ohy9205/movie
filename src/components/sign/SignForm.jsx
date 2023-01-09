8532eeae59200bff466857cf8f832fe5cf4bc44e
yeon <ohy9205@naver.com>
2023-01-09 10:33:16 +0900

    로그인/회원가입 스타일링

--- src/components/sign/SignForm.jsx
+++ src/components/sign/SignForm.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router";
 import { Link } from "react-router-dom";
 import { createUser, loginUser } from "../../api/firebase";
 import Button from "../ui/Button";
+import styles from "./SignForm.module.css";
 
 export default function SignForm({ isLogin }) {
   const [user, setUser] = useState({});
@@ -35,30 +36,32 @@ export default function SignForm({ isLogin }) {
   };
 
   return (
-    <div>
+    <div className={styles.sign}>
       <h1>{isLogin ? "로그인" : "회원가입"}</h1>
-      <form onSubmit={onSubmitHandler}>
-        <input
-          ref={emailRef}
-          type="email"
-          name="email"
-          value={user.email || ""}
-          onChange={onChangeHandler}
-          autoComplete="off"
-          placeholder="아이디"
-          required
-        />
-        <input
-          ref={passwordRef}
-          type="password"
-          name="password"
-          value={user.password || ""}
-          onChange={onChangeHandler}
-          autoComplete="off"
-          placeholder="비밀번호"
-          minLength="6"
-          required
-        />
+      <form className={styles.signForm} onSubmit={onSubmitHandler}>
+        <div className={styles.inputBox}>
+          <input
+            ref={emailRef}
+            type="email"
+            name="email"
+            value={user.email || ""}
+            onChange={onChangeHandler}
+            autoComplete="off"
+            placeholder="이메일"
+            required
+          />
+          <input
+            ref={passwordRef}
+            type="password"
+            name="password"
+            value={user.password || ""}
+            onChange={onChangeHandler}
+            autoComplete="off"
+            placeholder="비밀번호"
+            minLength="6"
+            required
+          />
+        </div>
         {!isLoading && <Button text={btnText} />}
         {isLoading && <p>Sending request...</p>}
         {error && <p>{error}</p>}
